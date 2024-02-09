d042037a74ffbadd6e7be7cab223061dfbcdde8f
Valentin Harrang <valhrg@gmail.com>
2024-02-09 20:03:13 +0100

    feat: replace "Rechercher" by an icon

--- components/organisms/SearchBar/index.tsx
+++ components/organisms/SearchBar/index.tsx
@@ -66,7 +66,20 @@ const SearchBar: FC = () => {
           onClick={handleSearch}
           className="ml-2 bg-blue-500 text-white px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent hover:bg-blue-600 transition-colors"
         >
-          Rechercher
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+            />
+          </svg>
         </button>
       </div>
 
